8792004928c4a67e85354025e8d8c73f432890e1
Viacheslav Chernogorov <slavachernogorv@mail.ru>
2023-02-28 11:09:21 +0300

    feat(Head):
    - use teg Head in Home page.
    
    meta: future

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import Link from 'next/link';
 import Image from 'next/image';
 
@@ -5,14 +6,20 @@ import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <h1 className={`${styles.title} font-effect-fire-animation`}>Главная</h1>
-      <div className={styles.mainImage}>
-        <Image src='/fatburger.png' alt='Burger' width={400} height={300} />
+    <>
+      <Head>
+        <title>Классные бургеры</title>
+        <meta name="title" content='Классные бургеры'/>
+      </Head>
+      <div className={styles.container}>
+        <h1 className={`${styles.title} font-effect-fire-animation`}>Главная</h1>
+        <div className={styles.mainImage}>
+          <Image src='/fatburger.png' alt='Burger' width={400} height={300} />
+        </div>
+        <p className={styles.text}>Что такое идеальный бургер? Лист свежего салата, мягкие булочки, сочное мясо. О других составляющих начинки можно поспорить, ведь это дело вкуса.</p>
+        <p className={styles.text}>Есть ещё пара-тройка факторов, влияющих на аппетит: цены, качество обслуживания, правильная атмосфера заведения.</p>
+        <Link href='/burgers' className={styles.btn}>Все бургеры</Link>
       </div>
-      <p className={styles.text}>Что такое идеальный бургер? Лист свежего салата, мягкие булочки, сочное мясо. О других составляющих начинки можно поспорить, ведь это дело вкуса.</p>
-      <p className={styles.text}>Есть ещё пара-тройка факторов, влияющих на аппетит: цены, качество обслуживания, правильная атмосфера заведения.</p>
-      <Link href='/burgers' className={styles.btn}>Все бургеры</Link>
-    </div>
+    </>
   );
 }
